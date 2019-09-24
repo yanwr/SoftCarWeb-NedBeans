ef4fca02d4028bcaa828c0337685b2439cd85f84
yanwr <yanywr@gmail.com>
2019-09-24 16:48:34 -0300

    Add

---
+++ src/java/br/com/classes/PostarCorrida.java
@@ -0,0 +1,87 @@
+
+package br.com.classes;
+
+public class PostarCorrida {
+    private Usuario motorista;
+    private String hrSaida;
+    private String hrPostada;
+    private String destino;
+    private String saida;
+    private int lugares;
+    private float taxa;
+
+    public PostarCorrida(Usuario motorista, String hrSaida, String hrPostada, String destino, String saida, int lugares, float taxa) {
+        this.motorista = motorista;
+        this.hrSaida = hrSaida;
+        this.hrPostada = hrPostada;
+        this.destino = destino;
+        this.saida = saida;
+        this.lugares = lugares;
+        this.taxa = taxa;
+    }
+
+    public PostarCorrida() {
+    }
+   
+    public String getHrSaida() {
+        return hrSaida;
+    }
+
+    public void setHrSaida(String hrSaida) {
+        this.hrSaida = hrSaida;
+    }
+
+    public String getHrPostada() {
+        return hrPostada;
+    }
+
+    public void setHrPostada(String hrPostada) {
+        this.hrPostada = hrPostada;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+
+    public String getSaida() {
+        return saida;
+    }
+
+    public void setSaida(String saida) {
+        this.saida = saida;
+    }
+
+    public int getLugares() {
+        return lugares;
+    }
+
+    public void setLugares(int lugares) {
+        this.lugares = lugares;
+    }
+
+    public float getTaxa() {
+        return taxa;
+    }
+
+    public void setTaxa(float taxa) {
+        this.taxa = taxa;
+    }
+
+    public Usuario getMotorista() {
+        return motorista;
+    }
+
+    public void setMotorista(Usuario motorista) {
+        this.motorista = motorista;
+    }
+
+    @Override
+    public String toString() {
+        return "PostarCorrida{" + "motorista=" + motorista + ", hrSaida=" + hrSaida + ", hrPostada=" + hrPostada + ", destino=" + destino + ", saida=" + saida + ", lugares=" + lugares + ", taxa=" + taxa + '}';
+    }
+    
+}
---
+++ src/java/br/com/classes/Usuario.java
@@ -0,0 +1,79 @@
+
+package br.com.classes;
+
+public class Usuario {
+    
+    private String nomecompleto;
+    private String profissao;
+    private String tempoTrabalhado;
+    private String nmrcel;
+    private String foto;
+    private int idade;
+
+    public Usuario(String nomecompleto, String profissao, String tempoTrabalhado, String nmrcel, String foto, int idade) {
+        this.nomecompleto = nomecompleto;
+        this.profissao = profissao;
+        this.tempoTrabalhado = tempoTrabalhado;
+        this.nmrcel = nmrcel;
+        this.foto = foto;
+        this.idade = idade;
+    }
+
+    public Usuario() {
+    }
+
+    public String getNomecompleto() {
+        return nomecompleto;
+    }
+
+    public void setNomecompleto(String nomecompleto) {
+        this.nomecompleto = nomecompleto;
+    }
+
+    public String getProfissao() {
+        return profissao;
+    }
+
+    public void setProfissao(String profissao) {
+        this.profissao = profissao;
+    }
+
+    public String getTempoTrabalhado() {
+        return tempoTrabalhado;
+    }
+
+    public void setTempoTrabalhado(String tempoTrabalhado) {
+        this.tempoTrabalhado = tempoTrabalhado;
+    }
+
+    public String getNmrcel() {
+        return nmrcel;
+    }
+
+    public void setNmrcel(String nmrcel) {
+        this.nmrcel = nmrcel;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" + "nomecompleto=" + nomecompleto + ", profissao=" + profissao + ", tempoTrabalhado=" + tempoTrabalhado + ", nmrcel=" + nmrcel + ", foto=" + foto + ", idade=" + idade + '}';
+    }
+    
+    
+}
